89629677496d4ca54b080ad3f2cca45ffe4620d1
Jairwin <jairwin@jairwin.cn>
2022-06-18 16:32:19 +0800

    test: formatDate单测调整

--- src/formatDate.ts
+++ src/formatDate.ts
@@ -2,10 +2,6 @@ import dayjs from 'dayjs';
 // 获取当前系统时间戳
 const timestamp = new Date().valueOf();
 
-// 是否有效日期
-function isValidDate(date: number): boolean {
-  return dayjs(date).isValid();
-}
 /**
  * 时间戳格式化输出
  * @param {string} timeStamp=timestamp 日期
@@ -16,14 +12,15 @@ function isValidDate(date: number): boolean {
  *
  * import { formatDate } from 'jairwin-utils/esm/formatDate';
  *
- * formatDate(new Date().valueOf());
+ * formatDate();
  *
  */
 
 export function formatDate(timeStamp: number = timestamp, format = 'YYYY-MM-DD HH:mm:ss'): string {
-  // 非时间戳格式，直接输出
-  if (!isValidDate(timeStamp)) {
-    console.log(`非时间戳格式，直接输出----->：`, timeStamp);
+  // 非number类型时间戳格式，直接输出
+  if (typeof timeStamp !== 'number') {
+    // eg：node：1655-02-08 00:00:00；brower：1659-07-22 23:24:07
+    console.log('非number类型时间戳格式，直接输出----->：', timeStamp);
     return String(timeStamp);
   }
   return dayjs(timeStamp).format(format);
--- test/[esm]/formatDate.test.js
+++ test/[esm]/formatDate.test.js
@@ -1,50 +1,67 @@
 import { expect } from 'vitest';
 import { formatDate } from '../../esm/formatDate';
 
-const timestamp = 1655020800000;
+const timeValue = 1655020800000;
 
 describe('[esm]formatDate', () => {
-  it('验证某一时间戳：1655112036395', () => {
-    const input = timestamp;
+  it('验证某一时间戳：1655020800000', () => {
+    const input = timeValue;
     const actual = formatDate(input, 'YYYY-MM-DD HH:mm:ss');
     const expected = '2022-06-12 16:00:00';
     expect(actual).toEqual(expected);
   });
-  it('验证默认时间戳', () => {
+  it('验证当前系统时间戳', () => {
     const input = new Date().valueOf();
     const actual = formatDate(input, 'YYYY-MM-DD HH:mm:ss');
     expect(actual).to.be.ok;
   });
   it('验证字符串时间戳', () => {
     const input = String(new Date().valueOf());
-    const actual = formatDate(input, 'YYYY-MM-DD HH:mm:ss');
-    expect(actual).to.be.ok;
+    const actual = formatDate(input);
+		expect(actual).toEqual(input);
   });
   it('验证默认时间格式', () => {
-    const input = timestamp;
+    const input = timeValue;
     const actual = formatDate(input);
     const expected = '2022-06-12 16:00:00';
     expect(actual).toEqual(expected);
   });
+	it('验证指定时间格式：2022-06-12 16:00:00', () => {
+    const input = timeValue;
+    const actual = formatDate(input, 'YYYY-MM-DD HH:mm:ss');
+    const expected = '2022-06-12 16:00:00';
+    expect(actual).toEqual(expected);
+  });
+	it('验证指定时间格式：2022-06-12 16:00', () => {
+    const input = timeValue;
+    const actual = formatDate(input, 'YYYY-MM-DD HH:mm');
+    const expected = '2022-06-12 16:00';
+    expect(actual).toEqual(expected);
+  });
   it('验证指定时间格式：2022-06-12', () => {
-    const input = timestamp;
+    const input = timeValue;
     const actual = formatDate(input, 'YYYY-MM-DD');
     const expected = '2022-06-12';
     expect(actual).toEqual(expected);
   });
   it('验证指定时间格式：06-12', () => {
-    const input = timestamp;
+    const input = timeValue;
     const actual = formatDate(input, 'MM-DD');
     const expected = '06-12';
     expect(actual).toEqual(expected);
   });
   it('验证指定时间格式：16:00', () => {
-    const input = timestamp;
+    const input = timeValue;
     const actual = formatDate(input, 'HH:mm');
     const expected = '16:00';
     expect(actual).toEqual(expected);
   });
-  it('验证无效格式时间', () => {
+	it('验证字符串时间戳格式', () => {
+    const input = String(timeValue);
+    const actual = formatDate(input);
+    expect(actual).toEqual(input);
+  });
+  it('验证非时间戳格式', () => {
     const input = '2022年6月10日 15:30:00';
     const actual = formatDate(input, 'YYYY-MM-DD HH:mm:ss');
     const expected = '2022年6月10日 15:30:00';
